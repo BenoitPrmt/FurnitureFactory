2cbd89bf2399e3cb60b3d9b2d63e67df61df302a
BenoitPrmt <benoitparmentier45@gmail.com>
2024-01-18 11:52:12 +0100

    added Factory class

--- index.php
+++ index.php
@@ -1,5 +1,24 @@
 <?php
 
+class Factory {
+
+    public array $inventory;
+
+    public function makeFurniture(string $type, array $data): void
+    {
+        if ($type === "table")
+        {
+            $this->inventory[] = new Table($data["name"], $data["reference"], $data["price"], $data["color"]);
+        }
+    }
+
+    public function getInventory(): array
+    {
+        return $this->inventory;
+    }
+
+}
+
 class Component
 {
     public string $name;
@@ -23,7 +42,7 @@ class Component
 
     public function __toString(): string
     {
-        return "Nom: " . $this->getName() ."<br>Reference: " . $this->getReference() . "<br>Prix: " . $this->getPrice() . "<br>Couleur: " . $this->getColor() . "<br>";
+        return "Nom: " . $this->getName() . "<br>Reference: " . $this->getReference() . "<br>Prix: " . $this->getPrice() . "<br>Couleur: " . $this->getColor() . "<br>";
     }
 
     public function getName(): string
@@ -69,15 +88,14 @@ class Component
 
 }
 
-class Table
+class Furniture
 {
-    private const int TABLE_LEG_QUANTITY = 4;
-    private const int PLATE_QUANTITY = 1;
 
     public string $name;
     public int $reference;
     public int $price;
     public string $color;
+    protected array $components = [];
 
     /**
      * @param string $name
@@ -93,6 +111,16 @@ class Table
         $this->setColor($color);
     }
 
+    public function __toString(): string
+    {
+        return "Nom: " . $this->getName() . "<br>Reference: " . $this->getReference() . "<br>Prix: " . $this->getPrice() . "<br>Couleur: " . $this->getColor() . "<br>";
+    }
+
+    public function getFurnitureInventory(): array
+    {
+        return $this->components;
+    }
+
     public function getName(): string
     {
         return $this->name;
@@ -134,10 +162,42 @@ class Table
     }
 }
 
+class Table extends Furniture
+{
+    private const int TABLE_LEG_QUANTITY = 4;
+    private const int PLATE_QUANTITY = 1;
+
+
+    public function __construct(string $name, int $reference, int $price, string $color)
+    {
+        parent::__construct($name, $reference, $price, $color);
+        $this->buildTable();
+    }
+
+    public function buildTable(): void
+    {
+        for ($i = 0; $i < self::TABLE_LEG_QUANTITY; ++$i) {
+            $this->components[] = new Component("Pied de table", 1234, 10, "Marron");
+        }
+        $this->components[] = new Component("Plateau", 98765, 30, $this->getColor());
+    }
+
+}
+
 class Chair
 {
 
 }
 
-$pied = new Component("Pied de table", 12345, 10, "Marron");
-echo $pied;
+//$pied = new Component("Pied de table", 12345, 10, "Marron");
+//echo $pied;
+
+$ikea = new Factory();
+$ikea->makeFurniture("table", [
+    'name' => "Table en bois",
+    'reference' => 5678,
+    'price' => 100,
+    'color' => "Marron"
+]);
+
+var_dump($ikea->getInventory());
